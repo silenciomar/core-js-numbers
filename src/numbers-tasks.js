47fad287b38e6788f81992816ffa2853b0523a02
silenciomar <96017754+silenciomar@users.noreply.github.com>
2023-11-17 23:08:34 +0600

    feat: add some solutions and drafts

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -112,8 +112,11 @@ function getLinearEquationRoot(a, b) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  return Math.acos(
+    (x1 * x2 + y1 * y2) /
+      (Math.sqrt(x1 ** 2 + y1 ** 2) * Math.sqrt(x2 ** 2 + y2 ** 2))
+  );
 }
 
 /**
@@ -129,8 +132,11 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+
+function getLastDigit(value) {
+  // const numToString = value.toString();
+  // return +numToString[numToString.length - 1];
+  return value % 10;
 }
 
 /**
@@ -144,8 +150,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return value * 1;
 }
 
 /**
@@ -161,8 +167,8 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 /**
@@ -203,8 +209,14 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
+
 function isPrime(/* n */) {
   throw new Error('Not implemented');
+  // let result;
+  // if (n % n === 0 && n / 1 === n) {
+  //   result = false;
+  // }
+  // return result;
 }
 
 /**
@@ -222,8 +234,12 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  // if (typeof value !== 'number') {
+  //   const newValue = Number(value);
+  //   return typeof newValue === 'number' ? newValue : def;
+  // }
+  // return def;
 }
 
 /**
